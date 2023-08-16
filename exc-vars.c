8dd5f33efb4d6147a2d36a64c9ebb8e68885d85c
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-08-16 11:18:48 +0700

    see the size of each data type

--- exc-vars.c
+++ exc-vars.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
+/*
+ * create makro/func to print variable size
+*/
+#define print_type_size(t) do {\
+    printf("Ukuran dari '" #t "' adalah %ld byte\n", sizeof(t));\
+}while(0);
+
 int main(void) {
-    int age;
-    
-    age = 47;
-    age = 77;
-    printf("text %d\n", age);
+    print_type_size(char);
+    print_type_size(short);
+    print_type_size(int);
+    print_type_size(long);
+    print_type_size(long long);
+    print_type_size(float);
+    print_type_size(double);
     return 0;
 }
